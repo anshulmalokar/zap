0613a99e509c6bb07d2b2cd815d2675bbaaaeeca
anshulmalokar <anshulmalokar@gmail.com>
2025-02-16 22:01:22 +0530

    fe-restart

--- frontend/src/app/_components/NavBar.tsx
+++ frontend/src/app/_components/NavBar.tsx
@@ -1,3 +1,5 @@
+'use client'
+import Link from "next/link";
 import React from "react";
 import { CgMoreVerticalO } from "react-icons/cg";
 import { IoMenuSharp } from "react-icons/io5";
@@ -19,12 +21,14 @@ export default function NavBar({}: Props) {
           <CgMoreVerticalO
           size={30}
           className="m-3 hover:bg-slate-100 hover:rounded-2xl" />
+          <Link href={"/signup"}>
           <button
             type="button"
             className="p-3 bg-orange-500 rounded-full "
           >
             Signup
           </button>
+          </Link>
         </div>
       </div>
     </>
--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,20 +1,115 @@
-import React from 'react'
+"use client";
+import React, { useRef } from "react";
+import axios from "axios";
+type Props = {};
 
-type Props = {}
-
-export default function page({}: Props) {
+export default function Page({}: Props) {
   return (
     <>
-        <div className='h-screen w-full bg-red-200'>
-            <div className= "bg-amber-900 ml-56 mr-56 h-screen flex">
-                <div className='w-1/2 h-full bg-slate-700'>
-
-                </div>
-                <div className='w-1/2 h-full bg-slate-300'>
-
-                </div>
-            </div>
+      <div className="h-[calc(100vh-4rem)] w-full flex justify-center items-center">
+        <div className="flex w-full h-full">
+          <div className="flex-1 flex justify-center items-center">
+            <LeftComponent />
+          </div>
+          <div className="flex-1 flex justify-center items-center">
+            <RightComponent/>
+          </div>
         </div>
+      </div>
     </>
-  )
+  );
+}
+
+function LeftComponent({}: Props) {
+  return (
+    <div className="flex flex-col justify-center items-start w-96">
+      <h1 className="text-3xl mb-6 font-bold">
+        Join millions worldwide who automate their work using Zapier.
+      </h1>
+      <h1 className="text-xl mb-2">Easy setup, no coding required</h1>
+      <h1 className="text-xl mb-2">Free forever for core features</h1>
+      <h1 className="text-xl">14-day trial of premium features & apps</h1>
+    </div>
+  );
+}
+
+function RightComponent({}: Props) {
+  const username = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const handleClickSignUp = async() => {
+    const body ={
+      username,
+      email,
+      password
+    }
+    const response = await axios.post(`${process.env.BACKEND_URL}/signup`,body);
+    if(response.status === 200){
+      const token = response.data.token;
+      localStorage.setItem("Authorization", `Bearer ${token}`);
+    }else{
+      alert("Error doing signup");
+    }
+  }
+  return (
+    <div className="flex flex-col justify-center items-center h-[calc(100vh-4rem)]">
+      <form
+        action="#"
+        onSubmit={e => e.preventDefault()}
+        className="w-96 max-w-md p-6 z-10 bg-white shadow-md rounded-lg border border-gray-200"
+      >
+        <h2 className="text-2xl font-bold text-center mb-6">Sign In</h2>
+
+        <div className="mb-4">
+          <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+            Username
+          </label>
+          <input
+            ref={username}
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Enter your username"
+            className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            Email
+          </label>
+          <input
+            ref={email}
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter your email"
+            className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+          />
+        </div>
+
+        <div className="mb-6">
+          <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+            Password
+          </label>
+          <input
+            ref={password}
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+            className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+          />
+        </div>
+
+        <button
+          onClick={handleClickSignUp}
+          type="submit"
+          className="w-full py-2 px-4 bg-orange-600 text-white font-semibold rounded-md hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-opacity-50"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
